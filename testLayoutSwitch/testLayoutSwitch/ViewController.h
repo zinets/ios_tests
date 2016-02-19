29355201df2a62fcff20b81df341a0548997efbb
Zinets Victor <victor.zinets@together.com>
2016-02-19 11:18:20 +0200

    ++ project for testing layout switch

---
+++ testLayoutSwitch/testLayoutSwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testLayoutSwitch
+//
+//  Created by Zinets Victor on 2/19/16.
+//  Copyright © 2016 Zinets Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
