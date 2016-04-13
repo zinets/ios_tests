1d2ca90bb8da6093024c3f6456f908a8af413080
Zinets Victor <victor.zinets@together.com>
2016-04-13 16:45:25 +0300

    ++

--- testPushController/testPushController/ViewController.m
+++ testPushController/testPushController/ViewController.m
@@ -33,15 +33,38 @@
         _scroller = [[UIScrollView alloc] initWithFrame:self.view.bounds];
         _scroller.pagingEnabled = YES;
         _scroller.contentSize = (CGSize){self.view.bounds.size.width, self.view.bounds.size.height * 2};
+        
+        _scroller.delegate = self;
     }
     return _scroller;
 }
 
 -(UIView *)fakePhotoView {
     if (!_fakePhotoView) {
-        _fakePhotoView = [[UILabel alloc] initWithFrame:self.view.bounds];
+        CGRect frm = self.view.bounds;
+        frm.size.height -= 64;
+        frm.origin.y = 64;
+        _fakePhotoView = [[UILabel alloc] initWithFrame:frm];
         borderControlWithParams(_fakePhotoView, 2, 0x7fff0000);
         ((UILabel *)_fakePhotoView).text = @"fake photo scroller here";
+        
+        UIImageView *iv = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"t1"]];
+        frm = iv.frame;
+        frm.origin = (CGPoint){20, 70};
+        iv.frame = frm;
+        [_fakePhotoView addSubview:iv];
+        
+        iv = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"t2"]];
+        frm = iv.frame;
+        frm.origin = (CGPoint){120, 270};
+        iv.frame = frm;
+        [_fakePhotoView addSubview:iv];
+        
+        iv = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"t3"]];
+        frm = iv.frame;
+        frm.origin = (CGPoint){50, 400};
+        iv.frame = frm;
+        [_fakePhotoView addSubview:iv];
     }
     return _fakePhotoView;
 }
@@ -49,7 +72,8 @@
 -(UITableView *)table {
     if (!_table) {
         CGRect frm = self.view.bounds;
-        frm.origin.y += frm.size.height;
+        frm.origin.y += frm.size.height + 64;
+        frm.size.height -= 64;
         _table = [[UITableView alloc] initWithFrame:frm style:(UITableViewStylePlain)];
         _table.backgroundColor = [UIColor colorWithHex:0xff4040];
         
@@ -96,6 +120,16 @@
     }
     
     [self.navigationController pushViewController:ctrl animated:YES];
+}
+
+#pragma mark - scroller
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (scrollView == _scroller) {
+        CGRect frm = self.fakePhotoView.frame;
+        frm.origin = (CGPoint){0, _scroller.contentOffset.y};
+        self.fakePhotoView.frame = frm;
+    }
 }
 
 @end
