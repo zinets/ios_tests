72c953282671b782b30963a8a85e320b52e78d8f
Zinets Victor <victor.zinets@together.com>
2016-04-14 11:41:09 +0300

    ++

---
+++ testPushController/testPushController/NavigationController.h
@@ -0,0 +1,14 @@
+//
+//  NavigationController.h
+//  testPushController
+//
+//  Created by Zinets Victor on 4/14/16.
+//  Copyright © 2016 Zinets Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface NavigationController : UINavigationController <UINavigationControllerDelegate>
+
+@end
