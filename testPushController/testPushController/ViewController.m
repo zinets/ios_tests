637f1a3082f3f2fdb8c0187372e858e306101b29
Zinets Victor <victor.zinets@together.com>
2016-04-14 15:34:24 +0300

    ++

--- testPushController/testPushController/ViewController.m
+++ testPushController/testPushController/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import "PushAnimator.h"
 
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (nonatomic, strong) UIScrollView *scroller;
