cc5678e4d581fc724b810c0fe4392e315e2f41f2
Zinets Victor <victor.zinets@together.com>
2016-09-28 15:15:48 +0300

    ++ черновой черновик интерфейса камеры

--- testPhotoSelector/TakePhotoControllerViewController.m
+++ testPhotoSelector/TakePhotoControllerViewController.m
@@ -20,70 +20,70 @@
 @implementation TakePhotoControllerViewController
 
 #pragma mark -
-- (void)viewDidLoad
-{
+
+- (void)viewDidLoad {
     [super viewDidLoad];
     self.view.frame = [UIScreen mainScreen].bounds;
     self.view.backgroundColor = [UIColor blackColor];
 
     CGRect frame = CGRectMake(0, 0, self.view.bounds.size.width, 51);
     UIView *topView = [[UIView alloc] initWithFrame:frame];
-    topView.backgroundColor = [UIColor yellowColor];
-#warning design setup //(!is35InchScreen())?[AppDesigner colorFor:iePhotoScreenBars]:[UIColor clearColor];
+    topView.backgroundColor = [UIColor clearColor];
     [self.view addSubview:topView];
     
-#warning design setup    InterfaceElement el = (!is35InchScreen())?iePhotoScreenGridButtonNoBg:iePhotoScreenGridButton;
-//    ColorButton *gridButton = [ColorButton buttonWithPresetsFor:el target:self action:@selector(onGrid)];
-//	[gridButton sizeToFit];
-//    if(is35InchScreen())gridButton.width = 49;
-//    gridButton.left = (!is35InchScreen())?41:10;
-//    gridButton.top = (topView.height - gridButton.height)/2;
     UIButton *gridButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
     gridButton.frame = (CGRect){{41, (topView.bounds.size.height - 35) / 2}, {49, 35}};
-    [gridButton setImage:[UIImage imageNamed:@"take-photo_transparent-button-icons_grid"]
-                forState:(UIControlStateNormal)];
-    [gridButton setBackgroundImage:[UIImage imageNamed:@"take-photo_trans-button-bg"]
-                          forState:(UIControlStateNormal)];
+    [gridButton setImage:[UIImage imageNamed:@"take-photo_transparent-button-icons_grid"] forState:(UIControlStateNormal)];
+    //  фоновая картинка была в 3.5"  [gridButton setBackgroundImage:[UIImage imageNamed:@"take-photo_trans-button-bg"] forState:(UIControlStateNormal)];
+    [gridButton addTarget:self action:@selector(onGrid) forControlEvents:(UIControlEventTouchUpInside)];
     [topView addSubview:gridButton];
-/*
-    ColorButton *switchCameraButton = [ColorButton buttonWithPresetsFor:(!is35InchScreen())?iePhotoScreenSwitchCameraButtonNoBg:iePhotoScreenSwitchCameraButton target:self action:@selector(onSwitchCamera)];
-	[switchCameraButton sizeToFit];
-    if(is35InchScreen())switchCameraButton.width = 49;
-    switchCameraButton.right = (!is35InchScreen())?topView.width - 41:topView.width - 10;
-    switchCameraButton.top = (topView.height - switchCameraButton.height)/2;
+    
+    UIButton *switchCameraButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    switchCameraButton.frame = (CGRect){{topView.bounds.size.width - 49 - 41, (topView.bounds.size.height - 35) / 2}, {49, 35}};
+    [switchCameraButton setImage:[UIImage imageNamed:@"take-photo_transparent-button-icons_flip"] forState:(UIControlStateNormal)];
+    //  фоновая картинка была в 3.5"  [switchCameraButton setBackgroundImage:[UIImage imageNamed:@"take-photo_trans-button-bg"] forState:(UIControlStateNormal)];
+    [switchCameraButton addTarget:self action:@selector(onSwitchCamera) forControlEvents:(UIControlEventTouchUpInside)];
     [topView addSubview:switchCameraButton];
-
-    _flashControl = [[FlirtCameraFlashControl alloc] initWithYOffset:(!is35InchScreen())?8:5];
+    
+    _flashControl = [[CameraFlashControl alloc] initWithYOffset:8];
+#warning как задаются размеры??
     [_flashControl addTarget:self action:@selector(onFlashModeAction:) forControlEvents:UIControlEventValueChanged];
     [topView addSubview:_flashControl];
-
-    frame = CGRectMake(0, 0, self.view.width, (!is35InchScreen())?90:53);
+    
+    frame = CGRectMake(0, self.view.bounds.size.height - 90, self.view.bounds.size.width, 90);
     UIView *bottomToolbarBg = [[UIView alloc] initWithFrame:frame];
-    bottomToolbarBg.bottom = self.view.height;
-    bottomToolbarBg.backgroundColor = [AppDesigner colorFor:iePhotoScreenBars];
+    bottomToolbarBg.backgroundColor = [UIColor blackColor]; // тут был "почти-чорный-как-цыган"
     [self.view addSubview:bottomToolbarBg];
 
-    ColorButton * doneButton = [ColorButton buttonWithPresetsFor:(!is35InchScreen()?iePhotoScreenTakePhotoButton:iePhotoScreenTakePhotoButton35Inch) target:self action:@selector(onDoneAction:)];
-    doneButton.center = CGPointMake(bottomToolbarBg.width/2, bottomToolbarBg.height/2);
+    UIButton *doneButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    [doneButton setImage:[UIImage imageNamed:@"take-photo-camera-icon"] forState:(UIControlStateNormal)];
+    doneButton.frame = (CGRect){{}, {70, 70}};
+    doneButton.center = CGPointMake(bottomToolbarBg.bounds.size.width / 2, bottomToolbarBg.bounds.size.height / 2);
+    doneButton.layer.cornerRadius = 35;
+    doneButton.backgroundColor = [UIColor greenColor];
+    [doneButton addTarget:self action:@selector(onDoneAction:) forControlEvents:(UIControlEventTouchUpInside)];
     [bottomToolbarBg addSubview:doneButton];
-
-    ColorButton *backButton = [[ColorButton buttonWithPresetsFor:iePhotoScreenBackButton target:self action:@selector(onBackAction)] setTitle:Localized(@"CANCEL")];
-	[backButton sizeToFit];
-    backButton.top = (bottomToolbarBg.height - backButton.height)/2;
-    backButton.left = 15;
+    
+    UIButton *backButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+#warning localize me!
+    [backButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18]];
+    [backButton setTitle:@"Cancel" forState:(UIControlStateNormal)];
+    [backButton sizeToFit];
+    [backButton addTarget:self action:@selector(onBackAction) forControlEvents:(UIControlEventTouchUpInside)];
+    backButton.center = (CGPoint){15 + backButton.bounds.size.width / 2, backButton.bounds.size.height / 2};
     [bottomToolbarBg addSubview:backButton];
-
-    frame = CGRectMake(0, (!is35InchScreen())?topView.bottom:0, self.view.width, (!is35InchScreen())?self.view.height - topView.bottom - bottomToolbarBg.height:self.view.height - bottomToolbarBg.height);
-*/
+    
+    frame = (CGRect){{0, topView.bounds.size.height}, {self.view.bounds.size.width, self.view.bounds.size.height - topView.bounds.size.height - bottomToolbarBg.bounds.size.height}};
+    
     UIImageView *outputView = [[UIImageView alloc] initWithFrame:frame];
     outputView.backgroundColor = [UIColor blackColor];
     [self.view insertSubview:outputView atIndex:0];
-/*
+
     _gridView = [[UIImageView alloc] initWithFrame:outputView.bounds];
-    _gridView.image = [AppDesigner imageFor:iePhotoScreenGrid];
+    _gridView.image = [UIImage imageNamed:@"take-photo_grid"];
     _gridView.hidden = YES;
     [outputView addSubview:_gridView];
-*/
+
     _cameraManager = [[CaptureSessionManager alloc] init];
     _cameraManager.imageOutputView = outputView;
 
@@ -95,7 +95,6 @@
 
     [self.navigationController setNavigationBarHidden:YES];
     [[UIApplication sharedApplication] setStatusBarHidden:YES];
-
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -110,40 +109,30 @@
     [_cameraManager stopSession];
 }
 
-- (void)viewDidDisappear:(BOOL)animated
-{
-    [super viewDidDisappear:animated];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-}
-
+#ifdef DEBUG
 - (void)dealloc
 {
     NSLog(@"%@ dealocated.", NSStringFromClass([self class]));
 }
+#endif
 
 #pragma mark - actions
--(void)onDoneAction:(id)sender {
 
+-(void)onDoneAction:(id)sender {
     __weak TakePhotoControllerViewController *bSelf = self;
     [_cameraManager takePhotoWithCompletionHandler:^(UIImage *image) {
         dispatch_async(dispatch_get_main_queue(), ^{
-            if(image)
-            {
-                [_cameraManager stopSession];
-                if([bSelf.delegate respondsToSelector:@selector(takePhotoController:didFinishPickingImage:)])
+            if (image) {
+                if ([bSelf.delegate respondsToSelector:@selector(takePhotoController:didFinishPickingImage:)]) {
+                    [_cameraManager stopSession];
                     [bSelf.delegate takePhotoController:self didFinishPickingImage:image];
+                }
             }
         });
     }];
-
 }
 
-- (void)onFlashModeAction:(CameraFlashControl *)sender
-{
+- (void)onFlashModeAction:(CameraFlashControl *)sender {
     switch (sender.cameraState) {
         case CameraFlashStateAuto:
             [_cameraManager setFlashMode:AVCaptureFlashModeAuto];
@@ -165,13 +154,11 @@
     _gridView.hidden = !_gridView.hidden;
 }
 
-- (void)onSwitchCamera
-{
+- (void)onSwitchCamera {
     _flashControl.hidden = ![_cameraManager switchCameras];
 }
 
-- (void)onBackAction
-{
+- (void)onBackAction {
     if([_delegate respondsToSelector:@selector(takePhotoControllerDidCancel:)])
         [_delegate takePhotoControllerDidCancel:self];
 }
