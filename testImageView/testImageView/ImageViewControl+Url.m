bfd3d10792dcbef82abb5612e84d64342c5341ab
Zinets Victor <victor.zinets@together.com>
2017-02-21 11:28:10 +0200

    ++ показ индикатора загрузки при длительном процессе (загрузка по сети)

--- testImageView/testImageView/ImageViewControl+Url.m
+++ testImageView/testImageView/ImageViewControl+Url.m
@@ -7,17 +7,33 @@
 @implementation ImageViewControl (Url)
 
 - (void)loadImageForUrl:(NSString *)imageUrl {
+    if (self.loadingView) {
+        self.userInteractionEnabled = NO;
+        self.loadingView.center = (CGPoint){CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds)};
+
+        [self addSubview:self.loadingView];
+    }
     [[TNImageDownloader sharedInstance] getImagesForUrls:@[imageUrl] observer:self];
 }
 
 #pragma mark - download delegation
 
 - (void)imageDownloader:(TNImageDownloader *)imageDownloader didDownloadImage:(UIImage *)image forUrl:(NSString *)imageUrl {
+    [self.loadingView removeFromSuperview];
+    self.userInteractionEnabled = YES;
+
     self.image = image;
 }
 
 - (void)imageDownloader:(TNImageDownloader *)imageDownloader didFailDownloadImageForUrl:(NSString *)imageUrl withError:(NSError *)error {
+    [self.loadingView removeFromSuperview];
+    self.userInteractionEnabled = YES;
+}
 
+- (void)imageDownloader:(TNImageDownloader *)imageDownloader didDownloadDataWithProgress:(float)progress {
+    if ([self.loadingView respondsToSelector:@selector(setProgress:animated:)]) {
+        [self.loadingView setProgress:progress animated:YES];
+    }
 }
 
 
