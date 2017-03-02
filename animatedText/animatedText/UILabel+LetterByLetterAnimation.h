bab7a0bcd039bbb2300e107a30ff4984df8cf5df
Zinets Victor <victor.zinets@together.com>
2017-03-02 11:02:13 +0200

    ++ финальный вариант анимации

--- animatedText/animatedText/UILabel+LetterByLetterAnimation.h
+++ animatedText/animatedText/UILabel+LetterByLetterAnimation.h
@@ -6,14 +6,14 @@
 #import <UIKit/UIKit.h>
 
 @interface UILabel (LetterByLetterAnimation)
-- (void)animateTextAppearing:(NSString *)newText charDelay:(NSTimeInterval)interval;
+- (void)animateTextAppearing:(NSString *)newText;
 @end
 
 @interface AnimatedTypiingLabel : UILabel {
 
 }
 @property (nonatomic) NSTimeInterval delayBetweenChars;
+/// цвет, который используется для анимированного проявления текста - по умолчанию будет равен цвету текста с прозрачностью 0,4
+@property (nonatomic, strong) UIColor *semiTransparentColor;
 - (void)setText:(NSString *)newText animated:(BOOL)animated;
-
-- (void)setText2:(NSString *)newText animated:(BOOL)animated;
 @end
--- animatedText/animatedText/UILabel+LetterByLetterAnimation.m
+++ animatedText/animatedText/UILabel+LetterByLetterAnimation.m
@@ -9,7 +9,7 @@
 
 @implementation UILabel (LetterByLetterAnimation)
 
-- (void)animateTextAppearing:(NSString *)newText charDelay:(NSTimeInterval)interval {
+- (void)animateTextAppearing:(NSString *)newText {
     self.text = @"";
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
@@ -23,7 +23,7 @@
                                          self.text = [self.text stringByAppendingString:substring];
                                      });
 
-                                     [NSThread sleepForTimeInterval:interval];
+                                     [NSThread sleepForTimeInterval:1/100.];
                                  }];
     });
 }
@@ -47,74 +47,60 @@
 }
 
 - (void)initComponent {
-    self.delayBetweenChars = 1 / 100.;
+    self.delayBetweenChars = 1 / 120.;
+    self.semiTransparentColor = [self.textColor colorWithAlphaComponent:0.4];
 }
 
 - (void)setText:(NSString *)newText animated:(BOOL)animated {
     if (animated) {
-        self.text = @"";
+        UIColor *textColor = self.textColor;
+        UIColor *semiColor = self.semiTransparentColor;
+        NSShadow *shadow1 = [[NSShadow alloc] init];
+        shadow1.shadowBlurRadius = 1;
 
+        NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:newText];
+        // сначала строка получает прозрачный цвет
+        [attributedString addAttribute:NSForegroundColorAttributeName
+                                 value:[UIColor clearColor]
+                                 range:NSMakeRange(0, newText.length)];
+        self.attributedText = attributedString;
+
+        // теперь к символам последовательно будет применяться нужный цвет с прозрачностью и полностью непрозрачный
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-            [newText enumerateSubstringsInRange:(NSRange){0, newText.length}
-                                        options:NSStringEnumerationByComposedCharacterSequences
-                                     usingBlock:^(NSString *substring,
-                                             NSRange substringRange,
-                                             NSRange enclosingRange,
-                                             BOOL *stop) {
-                                         dispatch_async(dispatch_get_main_queue(), ^{
-                                             self.text = [self.text stringByAppendingString:substring];
-                                         });
+            NSInteger textLength = newText.length;
+            for (int x = 0; x <= textLength; x++) {
+                // пусть раз из 3х будет добавляться сразу 3 символа
+                // но в начале текста пусть будет иногда добавляться только 1 символ
+                NSInteger range = arc4random() % 3 == 0 ? 3 : 2;
+                if (x < textLength / 5 || arc4random() % 3 == 0) {
+                    range = arc4random() % 3 == 0 ? 2 : 1;
+                }
 
-                                         [NSThread sleepForTimeInterval:self.delayBetweenChars];
-                                     }];
+                NSRange semiTransparentRange = (NSRange){x, MIN(range, textLength - x)}; {
+                    NSDictionary *attrs = @{NSForegroundColorAttributeName: semiColor,
+                            NSShadowAttributeName: shadow1};
+                    [attributedString addAttributes:attrs
+                                              range:semiTransparentRange];
+                }
+
+                NSRange opacityRange = (NSRange) {0, x}; {
+                    [attributedString addAttribute:NSForegroundColorAttributeName
+                                             value:textColor
+                                             range:opacityRange];
+                    [attributedString removeAttribute:NSShadowAttributeName
+                                                range:opacityRange];
+                }
+
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    self.attributedText = attributedString;
+                });
+
+                [NSThread sleepForTimeInterval:self.delayBetweenChars];
+            }
         });
     } else {
         [super setText:newText];
     }
-}
-
-- (void)setText2:(NSString *)newText animated:(BOOL)animated {
-    UIColor *textColor = self.textColor;
-    UIColor *semiColor = [textColor colorWithAlphaComponent:0.4];
-    NSShadow *shadow1 = [[NSShadow alloc] init];
-    shadow1.shadowBlurRadius = 1;
-
-    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:newText];
-    // сначала строка получает прозрачный цвет
-    [attributedString addAttribute:NSForegroundColorAttributeName
-                             value:[UIColor clearColor]
-                             range:NSMakeRange(0, newText.length)];
-    self.attributedText = attributedString;
-
-    // теперь к символам последовательно будет применяться нужный цвет с прозрачностью и полностью непрозрачный
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-        NSInteger textLength = newText.length;
-        for (int x = 0; x <= textLength; x++) {
-            NSRange semiTransparentRange = (NSRange){x, MIN(2, textLength - x)}; {
-                NSDictionary *attrs = @{NSForegroundColorAttributeName : semiColor,
-                NSShadowAttributeName : shadow1};
-                [attributedString addAttributes:attrs
-                                          range:semiTransparentRange];
-            }
-
-            NSRange opacityRange = (NSRange){0, x}; {
-                [attributedString addAttribute:NSForegroundColorAttributeName
-                                         value:textColor
-                                         range:opacityRange];
-                [attributedString removeAttribute:NSShadowAttributeName
-                                            range:opacityRange];
-            }
-
-            dispatch_async(dispatch_get_main_queue(), ^{
-                self.attributedText = attributedString;
-            });
-
-            [NSThread sleepForTimeInterval:self.delayBetweenChars];
-
-        }
-    });
-
-
 }
 
 @end
