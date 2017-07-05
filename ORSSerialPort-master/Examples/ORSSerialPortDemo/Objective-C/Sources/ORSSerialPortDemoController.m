3d8d9865b947abffcead9ba259186a76a95d1d66
Zinets Viktor <victor.zinets@together.com>
2017-07-05 10:27:14 +0300

    ++ скопом вместе с исходниками компонента

---
+++ ORSSerialPort-master/Examples/ORSSerialPortDemo/Objective-C/Sources/ORSSerialPortDemoController.m
@@ -0,0 +1,202 @@
+//
+//  ORSSerialPortDemoController.m
+//  ORSSerialPortDemo
+//
+//  Created by Andrew R. Madsen on 6/27/12.
+//	Copyright (c) 2012-2014 Andrew R. Madsen (andrew@openreelsoftware.com)
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the
+//	"Software"), to deal in the Software without restriction, including
+//	without limitation the rights to use, copy, modify, merge, publish,
+//	distribute, sublicense, and/or sell copies of the Software, and to
+//	permit persons to whom the Software is furnished to do so, subject to
+//	the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included
+//	in all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import "ORSSerialPortDemoController.h"
+#import "ORSSerialPortManager.h"
+
+@implementation ORSSerialPortDemoController
+
+#define LAST_PORT @"last_port"
+#define LAST_RATE @"last_rate"
+
+- (instancetype)init
+{
+	self = [super init];
+	if (self)
+	{
+		self.serialPortManager = [ORSSerialPortManager sharedSerialPortManager];
+		self.availableBaudRates = @[@300, @1200, @2400, @4800, @9600, @14400, @19200, @28800, @38400, @57600, @115200, @230400];
+		
+		NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+		[nc addObserver:self selector:@selector(serialPortsWereConnected:) name:ORSSerialPortsWereConnectedNotification object:nil];
+		[nc addObserver:self selector:@selector(serialPortsWereDisconnected:) name:ORSSerialPortsWereDisconnectedNotification object:nil];
+		
+#if (MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_7)
+		[[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
+#endif
+        id obj = [[NSUserDefaults standardUserDefaults] objectForKey:LAST_PORT];
+        if (obj) {
+            self.serialPort = [ORSSerialPort serialPortWithPath:obj];
+        }
+        obj = [[NSUserDefaults standardUserDefaults] objectForKey:LAST_RATE];
+        if (obj) {
+            self.serialPort.baudRate = obj;
+        }
+	}
+	return self;
+}
+
+- (void)dealloc
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark - Actions
+
+- (IBAction)send:(id)sender {
+    NSDate *d = [NSDate date];
+    NSTimeZone *tz = [NSTimeZone localTimeZone];
+    NSTimeInterval ut = round([d timeIntervalSince1970] + tz.secondsFromGMT);
+    NSString *string = [NSString stringWithFormat:@"t%@", @(ut)];
+	NSData *dataToSend = [string dataUsingEncoding:NSUTF8StringEncoding];
+	[self.serialPort sendData:dataToSend];
+}
+
+- (IBAction)openOrClosePort:(id)sender
+{
+	self.serialPort.isOpen ? [self.serialPort close] : [self.serialPort open];
+}
+
+#pragma mark - ORSSerialPortDelegate Methods
+
+- (void)serialPortWasOpened:(ORSSerialPort *)serialPort {
+	self.openCloseButton.title = @"Close";
+    [[NSUserDefaults standardUserDefaults] setObject:self.serialPort.path forKey:LAST_PORT];
+    [[NSUserDefaults standardUserDefaults] setObject:self.serialPort.baudRate forKey:LAST_RATE];
+}
+
+- (void)serialPortWasClosed:(ORSSerialPort *)serialPort
+{
+	self.openCloseButton.title = @"Open";
+}
+
+- (void)serialPort:(ORSSerialPort *)serialPort didReceiveData:(NSData *)data
+{
+	NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+	if ([string length] == 0) return;
+	[self.receivedDataTextView.textStorage.mutableString appendString:string];
+	[self.receivedDataTextView setNeedsDisplay:YES];
+}
+
+- (void)serialPortWasRemovedFromSystem:(ORSSerialPort *)serialPort;
+{
+	// After a serial port is removed from the system, it is invalid and we must discard any references to it
+	self.serialPort = nil;
+	self.openCloseButton.title = @"Open";
+}
+
+- (void)serialPort:(ORSSerialPort *)serialPort didEncounterError:(NSError *)error
+{
+	NSLog(@"Serial port %@ encountered an error: %@", serialPort, error);
+}
+
+#pragma mark - NSUserNotificationCenterDelegate
+
+#if (MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_7)
+
+- (void)userNotificationCenter:(NSUserNotificationCenter *)center didDeliverNotification:(NSUserNotification *)notification
+{
+	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, 3.0 * NSEC_PER_SEC);
+	dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+		[center removeDeliveredNotification:notification];
+	});
+}
+
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center shouldPresentNotification:(NSUserNotification *)notification
+{
+	return YES;
+}
+
+#endif
+
+#pragma mark - Notifications
+
+- (void)serialPortsWereConnected:(NSNotification *)notification
+{
+	NSArray *connectedPorts = [notification userInfo][ORSConnectedSerialPortsKey];
+	NSLog(@"Ports were connected: %@", connectedPorts);
+	[self postUserNotificationForConnectedPorts:connectedPorts];
+}
+
+- (void)serialPortsWereDisconnected:(NSNotification *)notification
+{
+	NSArray *disconnectedPorts = [notification userInfo][ORSDisconnectedSerialPortsKey];
+	NSLog(@"Ports were disconnected: %@", disconnectedPorts);
+	[self postUserNotificationForDisconnectedPorts:disconnectedPorts];
+	
+}
+
+- (void)postUserNotificationForConnectedPorts:(NSArray *)connectedPorts
+{
+#if (MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_7)
+	if (!NSClassFromString(@"NSUserNotificationCenter")) return;
+	
+	NSUserNotificationCenter *unc = [NSUserNotificationCenter defaultUserNotificationCenter];
+	for (ORSSerialPort *port in connectedPorts)
+	{
+		NSUserNotification *userNote = [[NSUserNotification alloc] init];
+		userNote.title = NSLocalizedString(@"Serial Port Connected", @"Serial Port Connected");
+		NSString *informativeTextFormat = NSLocalizedString(@"Serial Port %@ was connected to your Mac.", @"Serial port connected user notification informative text");
+		userNote.informativeText = [NSString stringWithFormat:informativeTextFormat, port.name];
+		userNote.soundName = nil;
+		[unc deliverNotification:userNote];
+	}
+#endif
+}
+
+- (void)postUserNotificationForDisconnectedPorts:(NSArray *)disconnectedPorts
+{
+#if (MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_7)
+	if (!NSClassFromString(@"NSUserNotificationCenter")) return;
+	
+	NSUserNotificationCenter *unc = [NSUserNotificationCenter defaultUserNotificationCenter];
+	for (ORSSerialPort *port in disconnectedPorts)
+	{
+		NSUserNotification *userNote = [[NSUserNotification alloc] init];
+		userNote.title = NSLocalizedString(@"Serial Port Disconnected", @"Serial Port Disconnected");
+		NSString *informativeTextFormat = NSLocalizedString(@"Serial Port %@ was disconnected from your Mac.", @"Serial port disconnected user notification informative text");
+		userNote.informativeText = [NSString stringWithFormat:informativeTextFormat, port.name];
+		userNote.soundName = nil;
+		[unc deliverNotification:userNote];
+	}
+#endif
+}
+
+
+#pragma mark - Properties
+
+- (void)setSerialPort:(ORSSerialPort *)port {
+    if (port != _serialPort) {
+		[_serialPort close];
+		_serialPort.delegate = nil;
+		
+		_serialPort = port;
+		
+		_serialPort.delegate = self;
+	}
+}
+
+@end
