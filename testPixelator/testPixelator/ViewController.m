defcd71d0ee6ec3a22de52012f276c17279dff9b
Zinets Viktor <victor.zinets@together.com>
2017-09-28 17:03:53 +0300

    +++

--- testPixelator/testPixelator/ViewController.m
+++ testPixelator/testPixelator/ViewController.m
@@ -20,10 +20,12 @@
     [super viewDidLoad];
 
     UIImage *img = [UIImage imageNamed:@"image.jpg"];
-    PixelateLayer *layer1 = [[PixelateLayer alloc] init:ShapeSquare resolution:10 size:10 offset:0 alpha:0.6];
+    PixelateLayer *layer1 = [[PixelateLayer alloc] init:ShapeSquare resolution:20 size:20 offset:0 alpha:0.8];
 //    PixelateLayer *layer2 = [[PixelateLayer alloc] init:ShapeSquare resolution:20 size:10 offset:5 alpha:0.6];
 
+    NSLog(@"1");
     UIImage *resImg = [Pixelator create:img layers:@[layer1]];
+    NSLog(@"2");
 
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:(CGRect){{20, 70}, {280, 280}}];
     imageView.contentMode = UIViewContentModeScaleAspectFill;
