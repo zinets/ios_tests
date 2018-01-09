a383d9daa94f11f4bbb6041896f93eb1b9c60432
Zinets Viktor <victor.zinets@together.com>
2018-01-09 10:54:22 +0200

    ++

--- testConversations/testConversations/classes/ConversationCell.h
+++ testConversations/testConversations/classes/ConversationCell.h
@@ -16,11 +16,15 @@ typedef enum {
 } ConversationCellType;
 
 @interface ConversationCell : UITableViewCell
+// todo remove date
 @property (nonatomic, strong) NSString *messageDate;
 
-@property (nonatomic) BOOL isOwnMessage;
 @property (nonatomic) ConversationCellType cellType;
 @property (nonatomic, strong) UIColor *balloonBackgroundColor;
+
+@property (nonatomic) BOOL isOwnMessage;
+// влияет на дизайн (чужого сообщения): балун сдвигается; появляется аватарка и скриннейм в 1й ячейке группы
+@property (nonatomic) BOOL isScreennameVisible;
 @end
 
 @interface ConversationMessageCell : ConversationCell
