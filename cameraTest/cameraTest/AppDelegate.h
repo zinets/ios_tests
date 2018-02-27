69dbd0f7b77f2b9f002bcdc95153a5272b1ee9b8
Victor Zinets <victor.zinets@together.com>
2018-02-27 11:47:25 +0200

    ++

---
+++ cameraTest/cameraTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cameraTest
+//
+//  Created by Victor Zinets on 2/27/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cameraTest/cameraTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cameraTest
+//
+//  Created by Victor Zinets on 2/27/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
