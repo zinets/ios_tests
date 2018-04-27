99f90887dd95f0ae2b33e8eb9f95ce70fd387814
Victor Zinets <victor.zinets@together.com>
2018-04-27 14:50:30 +0300

    ++

---
+++ pagerWithAnimations/pagerWithAnimations/NibLoadedView/RMNibLoadedView.m
@@ -0,0 +1,120 @@
+//
+//  RMNibLoadedView.m
+//  Pods
+//
+//  Created by pm on 02.09.16.
+//
+//
+
+#import "RMNibLoadedView.h"
+#import "RMCachingNibLoader.h"
+
+@interface RMNibLoadedView ()
+
+@property(nonatomic, weak) UIView *nibView;
+@property(nonatomic) BOOL needReload;
+
+@end
+
+@implementation RMNibLoadedView
+
+- (instancetype)init {
+    return  [self initWithFrame:CGRectZero];
+}
+- (instancetype)initWithFrame:(CGRect)frame
+             reloadingFromNib:(BOOL)reloadingFromNib {
+    self = [super initWithFrame:frame];
+    if (self && reloadingFromNib) {
+        self.needReload = reloadingFromNib;
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    BOOL reloadFromNib = YES;
+#if TARGET_INTERFACE_BUILDER
+    reloadFromNib = NO;
+#endif
+    return [self initWithFrame:frame
+              reloadingFromNib: reloadFromNib];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+                      nibName:(NSString *)nibName
+                 nibViewIndex:(NSInteger)nibViewIndex
+                     inBundle:(NSBundle *)bundle
+                      options:(NSDictionary *)options {
+   self = [self initWithFrame:frame
+             reloadingFromNib:NO];
+   if (self) {
+      self.nibName = nibName;
+      self.nibViewIndex = nibViewIndex;
+      self.nibBundle = bundle;
+      self.nibOptions = options;
+      [self commonInit];
+   }
+   return self;
+}
+
+- (void)prepareForInterfaceBuilder {
+   [super prepareForInterfaceBuilder];
+   self.needReload = YES;
+   [self reloadNibView];
+}
+
+- (void)awakeFromNib {
+   [super awakeFromNib];
+    [self.nibView awakeFromNib];
+   self.needReload = YES;
+   [self reloadNibView];
+}
+
+- (void)setNibView:(UIView *)nibView {
+   if (_nibView) {
+      [_nibView removeFromSuperview];
+   }
+   _nibView = nibView;
+}
+
+- (void)commonInit {
+    [self reloadNibView];
+}
+
+- (void)setNibViewIndex:(NSInteger)nibViewIndex {
+   _nibViewIndex = nibViewIndex;
+   self.needReload = YES;
+   [self reloadNibView];
+}
+
+- (void)setNibName:(NSString *)nibName {
+   _nibName = nibName;
+   self.needReload = YES;
+   [self reloadNibView];
+}
+
+- (NSBundle *)nibBundle {
+   if (!_nibBundle) {
+      _nibBundle = [NSBundle bundleForClass:self.class];
+   }
+   return _nibBundle;
+}
+
+- (void)reloadNibView {
+   if (self.needReload) {
+      UIView *nibView = [RMCachingNibLoader loadViewFromNibNamed:self.nibName
+                                                        inBundle:self.nibBundle
+                                                         atIndex:self.nibViewIndex
+                                                         options:self.nibOptions
+                                                      loaderView:self];
+      nibView.frame = self.bounds;
+      nibView.translatesAutoresizingMaskIntoConstraints = YES;
+      nibView.autoresizingMask =
+      (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+      [self addSubview:nibView];
+      self.nibView = nibView;
+      self.needReload = NO;
+   }
+}
+
+@end
---
+++ pagerWithAnimations/pagerWithAnimations/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  pagerWithAnimations
+//
+//  Created by Victor Zinets on 4/27/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
