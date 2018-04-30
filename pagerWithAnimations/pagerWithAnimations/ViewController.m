2c35c2aced5429ed1fa0f68f455568cd6d6fc2eb
Victor Zinets <victor.zinets@together.com>
2018-04-30 09:04:01 +0300

    ++

--- pagerWithAnimations/pagerWithAnimations/ViewController.m
+++ pagerWithAnimations/pagerWithAnimations/ViewController.m
@@ -7,11 +7,11 @@
 //
 
 #import "ViewController.h"
-#import "SPager.h"
+
 
 @interface ViewController ()
 @property (strong, nonatomic) IBOutletCollection(UIView) NSArray *boxes;
-@property (weak, nonatomic) IBOutlet SPager *pager;
+
 
 @end
 
@@ -20,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self removeBoxes];
+
 }
 
 - (IBAction)onTap:(id)sender {
@@ -29,16 +30,40 @@
     [self removeBoxes];
 }
 
+- (CGAffineTransform)rightTransform {
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    transform = CGAffineTransformTranslate(transform, 160, 0);
+    transform = CGAffineTransformRotate(transform, -M_PI * 1.09);
+    transform = CGAffineTransformScale(transform, 0.5, 0.5);
+
+    return transform;
+}
+
+- (CGAffineTransform)leftTransform {
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    transform = CGAffineTransformTranslate(transform, -160, 0);
+    transform = CGAffineTransformScale(transform, 0.5, 0.5);
+    transform = CGAffineTransformRotate(transform, -M_PI * .99);
+    
+    return transform;
+}
+
 -(void)addBoxes {
     [self.boxes enumerateObjectsUsingBlock:^(UIView *box, NSUInteger idx, BOOL *stop) {
-        box.transform = CGAffineTransformMakeTranslation(200, -15);
+        box.transform = [self rightTransform];
         box.alpha = 0;
-        [UIView animateWithDuration:0.4 delay:0.05 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        [UIView animateWithDuration:0.6
+                              delay:0.05 * idx
+             usingSpringWithDamping:0.95
+              initialSpringVelocity:20
+                            options:UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionBeginFromCurrentState
+                         animations:^{
             box.transform = CGAffineTransformIdentity;
             box.alpha = 1;
         } completion:^(BOOL finished) {
 
         }];
+        
     }];
 }
 
@@ -47,12 +72,50 @@
         box.transform = CGAffineTransformIdentity;
         box.alpha = 1;
 
-        [UIView animateWithDuration:0.4 delay:0.05 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-            box.transform = CGAffineTransformMakeTranslation(-200, -15);
+        [UIView animateWithDuration:.6
+                              delay:0.05 * idx
+             usingSpringWithDamping:0.95
+              initialSpringVelocity:20
+                            options:UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionBeginFromCurrentState
+                         animations:^{
+                             box.transform = [self leftTransform];
             box.alpha = 0;
         } completion:^(BOOL finished) {
 
         }];
+    }];
+}
+
+- (IBAction)addFromLeft {
+    [self changeTransformFrom:[self leftTransform] to:(CGAffineTransformIdentity)];
+}
+
+- (IBAction)addFromRight {
+    [self changeTransformFrom:[self rightTransform] to:(CGAffineTransformIdentity)];
+}
+- (IBAction)removeToRight:(id)sender {
+    [self changeTransformFrom:(CGAffineTransformIdentity) to:[self rightTransform]];
+}
+
+- (IBAction)removeToLeft:(id)sender {
+    [self changeTransformFrom:(CGAffineTransformIdentity) to:[self leftTransform]];
+}
+
+- (void)changeTransformFrom:(CGAffineTransform)fromTransform to:(CGAffineTransform)toTransform {
+    CGFloat fromAlpha = CGAffineTransformIsIdentity(fromTransform) ? 1 : 0;
+    CGFloat toAlpha = fromAlpha ? 0 : 1;
+    [self.boxes enumerateObjectsUsingBlock:^(UIView *box, NSUInteger idx, BOOL *stop) {
+        box.transform = fromTransform;
+        box.alpha =  fromAlpha;
+        [UIView animateWithDuration:0.6
+                              delay:0.05 * idx
+             usingSpringWithDamping:0.95
+              initialSpringVelocity:20
+                            options:UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionBeginFromCurrentState
+                         animations:^{
+                             box.transform = toTransform;
+                             box.alpha = toAlpha;
+                         } completion:nil];
     }];
 }
 
