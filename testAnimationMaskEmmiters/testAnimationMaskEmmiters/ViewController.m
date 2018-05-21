a217e7faba020709f44577534d1a1c57e70ecca7
Victor Zinets <victor.zinets@together.com>
2018-05-21 13:46:48 +0300

    ++

--- testAnimationMaskEmmiters/testAnimationMaskEmmiters/ViewController.m
+++ testAnimationMaskEmmiters/testAnimationMaskEmmiters/ViewController.m
@@ -9,12 +9,14 @@
 #import "ViewController.h"
 #import "AnimatedMaskView.h"
 #import "AnimatedMaskView2.h"
+#import "FavButton.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet AnimatedMaskView2 *previewImageView;
 @property (weak, nonatomic) IBOutlet AnimatedMaskView *i2;
 @property (weak, nonatomic) IBOutlet AnimatedMaskView *i3;
 
+@property (weak, nonatomic) IBOutlet FavButton *testFavButton;
 @end
 
 @implementation ViewController
@@ -42,6 +44,60 @@
 //    [self.previewImageView animate];
     [self.i2 animate];
     [self.i3 animate];
+}
+
+#pragma mark button animations -
+
+- (IBAction)onTap:(UIButton *)sender {
+    if (sender.selected) {
+        sender.selected = NO;
+        return;
+    }
+    
+    UIImageView *icon = [UIImageView new];
+    CGRect frm = [self.view convertRect:sender.imageView.frame fromView:sender];
+    icon.image = [sender imageForState:(UIControlStateHighlighted)];
+    icon.frame = frm;
+    [self.view addSubview:icon];
+    
+    CGPoint center = CGPointMake(CGRectGetMidX(self.view.frame), CGRectGetMidY(self.view.frame));
+    [UIView animateWithDuration:0.9 animations:^{
+        icon.center = center;
+    } completion:^(BOOL finished) {
+        [self startParticles:center];
+        [icon removeFromSuperview];        
+    }];
+    sender.selected = YES;
+}
+
+- (void)startParticles:(CGPoint)startPoint {
+    CAEmitterLayer *emitterLayer = [CAEmitterLayer layer];
+    emitterLayer.emitterPosition = startPoint;
+    emitterLayer.emitterZPosition = 10.0;
+    emitterLayer.emitterSize = CGSizeMake(0, 0);
+    emitterLayer.emitterShape = kCAEmitterLayerSphere;
+    emitterLayer.spin = 0.5;
+    
+    CAEmitterCell *emitterCell = [CAEmitterCell emitterCell];
+    
+    emitterCell.scale = 0.4;
+    emitterCell.scaleRange = 0.7;
+    
+    emitterCell.emissionRange = (CGFloat)M_PI;
+    
+    emitterCell.lifetime = 10.0;
+    emitterCell.birthRate = 4.0;
+    emitterCell.velocity = 100.0;
+    emitterCell.velocityRange = 100.0;
+    
+    emitterCell.xAcceleration = 100.0;
+    emitterCell.yAcceleration = -20.0;
+    
+    
+    emitterCell.contents = (id)[[UIImage imageNamed:@"favorite2_hlited"] CGImage];
+    
+    emitterLayer.emitterCells = @[emitterCell];
+    [self.view.layer addSublayer:emitterLayer];
 }
 
 @end
--- testAnimationMaskEmmiters/testAnimationMaskEmmiters/classes/FavButton.m
+++ testAnimationMaskEmmiters/testAnimationMaskEmmiters/classes/FavButton.m
@@ -7,15 +7,77 @@
 //
 
 #import "FavButton.h"
+@interface FavButton() <CAAnimationDelegate> {
+    CAShapeLayer *pulseLayer;
+    CALayer *iconLayer;
+}
+@end
 
 @implementation FavButton
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+-(instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    self.layer.masksToBounds = NO;
+    return self;
 }
-*/
+
+-(instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    self.clipsToBounds = NO;
+    self.layer.masksToBounds = NO;
+    return self;
+}
+
+-(void)setSelected:(BOOL)selected {
+    [pulseLayer removeFromSuperlayer];
+    [iconLayer removeFromSuperlayer];
+    
+    if (selected) {
+        // bg animation
+        pulseLayer = [CAShapeLayer layer];
+        pulseLayer.frame = [self bounds];
+        pulseLayer.fillColor = [UIColor redColor].CGColor;
+        
+        UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:pulseLayer.bounds];
+        pulseLayer.path = path.CGPath;
+        [self.layer addSublayer:pulseLayer];
+        
+        CAKeyframeAnimation *transform = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+        transform.delegate = self;
+        
+        NSMutableArray *values = [NSMutableArray new];
+        [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(0.1, 0.1, 1)]];
+        [values addObject:[NSValue valueWithCATransform3D:CATransform3DMakeScale(1.2, 1.2, 1)]];
+        [values addObject:[NSValue valueWithCATransform3D:CATransform3DIdentity]];
+        
+        transform.values = values;
+        transform.keyTimes = @[@0.0, @0.4, @1.0];
+        transform.removedOnCompletion = NO;
+        transform.fillMode = kCAFillModeForwards;
+        transform.duration = 0.5;
+        transform.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+        
+        [pulseLayer addAnimation:transform forKey:@"1"];
+        
+        // icon animation
+        iconLayer = [CALayer layer];
+        iconLayer.frame = self.bounds;
+        UIImage *img = [self imageForState:(UIControlStateSelected)];
+        iconLayer.contents = (id)img.CGImage;
+        iconLayer.contentsGravity = @"center";
+        iconLayer.contentsScale = 2;
+        [self.layer addSublayer:iconLayer];
+        
+    } else {
+        [super setSelected:selected];
+    }
+}
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    if (flag) {
+
+        [super setSelected:YES];
+    }
+}
 
 @end
