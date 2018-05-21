c2cfc9df8bab9ebe96bc867e63a18a509c3c7dbc
Victor Zinets <victor.zinets@together.com>
2018-05-21 13:46:32 +0300

    no message

---
+++ testAnimationMaskEmmiters/testAnimationMaskEmmiters/classes/FavButton.h
@@ -0,0 +1,13 @@
+//
+//  FavButton.h
+//  testAnimationMaskEmmiters
+//
+//  Created by Victor Zinets on 5/21/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavButton : UIButton
+
+@end
---
+++ testAnimationMaskEmmiters/testAnimationMaskEmmiters/classes/FavButton.m
@@ -0,0 +1,21 @@
+//
+//  FavButton.m
+//  testAnimationMaskEmmiters
+//
+//  Created by Victor Zinets on 5/21/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+#import "FavButton.h"
+
+@implementation FavButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
