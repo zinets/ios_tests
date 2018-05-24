cc5a59ae2a4408db6d4911c900839027a1956498
Victor Zinets <victor.zinets@together.com>
2018-05-24 10:22:09 +0300

    ++

---
+++ testAnimationMaskEmmiters/testAnimationMaskEmmiters/classes/GrayscaleImageView.h
@@ -0,0 +1,13 @@
+//
+//  GrayscaleImageView.h
+//  testAnimationMaskEmmiters
+//
+//  Created by Victor Zinets on 5/24/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+#import <TNURLImageView/TNURLImageView.h>
+
+@interface GrayscaleImageView : TNImageView
+
+@end
---
+++ testAnimationMaskEmmiters/testAnimationMaskEmmiters/classes/GrayscaleImageView.m
@@ -0,0 +1,21 @@
+//
+//  GrayscaleImageView.m
+//  testAnimationMaskEmmiters
+//
+//  Created by Victor Zinets on 5/24/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+#import "GrayscaleImageView.h"
+
+@implementation GrayscaleImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
