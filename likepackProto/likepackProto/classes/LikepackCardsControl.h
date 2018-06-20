b9d75336ef403140aa99152978ecfa75a850bfab
Victor Zinets <victor.zinets@together.com>
2018-06-20 15:11:47 +0300

    ++

---
+++ likepackProto/likepackProto/classes/LikepackCardsControl.h
@@ -0,0 +1,13 @@
+//
+//  LikepackCardsControl.h
+//  likepackProto
+//
+//  Created by Victor Zinets on 6/20/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LikepackCardsControl : UIView
+
+@end
---
+++ likepackProto/likepackProto/classes/LikepackCardsControl.m
@@ -0,0 +1,21 @@
+//
+//  LikepackCardsControl.m
+//  likepackProto
+//
+//  Created by Victor Zinets on 6/20/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+#import "LikepackCardsControl.h"
+
+@implementation LikepackCardsControl
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
