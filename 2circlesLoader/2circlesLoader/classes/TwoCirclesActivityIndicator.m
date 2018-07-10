e7a81e13332dd195f1fd95af798d38aeeea31791
Victor Zinets <victor.zinets@together.com>
2018-07-10 09:34:47 +0300

    ++ animation stages idea

--- 2circlesLoader/2circlesLoader/classes/TwoCirclesActivityIndicator.m
+++ 2circlesLoader/2circlesLoader/classes/TwoCirclesActivityIndicator.m
@@ -7,17 +7,22 @@
 
 typedef enum {
     TwoCirclesActivityAnimationStageStopped,
-    TwoCirclesActivityAnimationStatring,
+    TwoCirclesActivityAnimationStageStarting,
     TwoCirclesActivityAnimationStageRotating,
     TwoCirclesActivityAnimationStageStopping,
 } TwoCirclesActivityAnimationStage;
 
-@interface TwoCirclesActivityIndicator() {
+@interface TwoCirclesActivityIndicator() <CAAnimationDelegate> {
     TwoCirclesActivityAnimationStage animationStage;
 }
-@property (nonatomic, strong) CALayer *layer1;
-@end
+@property (nonatomic, strong) CAShapeLayer *layer1;
+@property (nonatomic, strong) CAShapeLayer *layer2;
 
+@property (nonatomic) CGFloat lineWidth;
+@property (nonatomic) CGFloat lineSpacing;
+@property (nonatomic, strong) UIColor *line1Color;
+@property (nonatomic, strong) UIColor *line2Color;
+@end
 
 @implementation TwoCirclesActivityIndicator {
 
@@ -25,21 +30,68 @@ typedef enum {
 
 #pragma mark getters -
 
-- (CALayer *)layer1 {
+- (CAShapeLayer *)layer1 {
     if (!_layer1) {
-        _layer1 = [CALayer new];
+        _layer1 = [CAShapeLayer new];
         CGRect frame = self.bounds;
         _layer1.frame = frame;
-        _layer1.backgroundColor = [UIColor yellowColor].CGColor;
+        _layer1.strokeColor = self.line1Color.CGColor;
+        _layer1.fillColor = [UIColor clearColor].CGColor;
+
+        CGPoint center = (CGPoint){self.bounds.size.width / 2, self.bounds.size.height / 2};
+        CGFloat radius = MIN(self.bounds.size.width, self.bounds.size.height) / 2;
+        UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:center
+                                                            radius:radius
+                                                        startAngle:M_PI_4
+                                                          endAngle:3 * M_PI_4
+                                                         clockwise:NO];
+        _layer1.strokeStart =
+        _layer1.strokeEnd = 0;
+        _layer1.lineWidth = self.lineWidth;
+        _layer1.lineCap = kCALineCapRound;
+
+        _layer1.path = path.CGPath;
     }
 
     return _layer1;
 }
 
+- (CAShapeLayer *)layer2 {
+    if (!_layer2) {
+        _layer2 = [CAShapeLayer new];
+        CGRect frame = self.bounds;
+        CGPoint center = (CGPoint){self.bounds.size.width / 2, self.bounds.size.height / 2};
+        CGFloat radius = MIN(self.bounds.size.width, self.bounds.size.height) / 2 - self.lineSpacing;
+        _layer2.frame = frame;
+        _layer2.strokeColor = self.line2Color.CGColor;
+        _layer2.fillColor = [UIColor clearColor].CGColor;
+
+        UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:center
+                                                            radius:radius
+                                                        startAngle:-M_PI_4
+                                                          endAngle:-3 * M_PI_4
+                                                         clockwise:YES];
+        _layer2.strokeStart = 0;
+        _layer2.strokeEnd = 0;
+        _layer2.lineWidth = self.lineWidth;
+        _layer2.lineCap = kCALineCapRound;
+
+        _layer2.path = path.CGPath;
+    }
+
+    return _layer2;
+}
+
 #pragma mark public -
 
 - (void)commonInit {
+    _lineWidth = 3;
+    _lineSpacing = _lineWidth * 3;
+    _line1Color = [UIColor lightGrayColor];
+    _line2Color = [UIColor lightGrayColor];
+
     [self.layer addSublayer:self.layer1];
+    [self.layer addSublayer:self.layer2];
 }
 
 - (instancetype)initWithFrame:(CGRect)frame {
@@ -47,7 +99,6 @@ typedef enum {
     if (self) {
         [self commonInit];
     }
-
     return self;
 }
 
@@ -56,22 +107,126 @@ typedef enum {
     if (self) {
         [self commonInit];
     }
-
     return self;
 }
 
 
 - (void)startAnimation {
-    CABasicAnimation *rotating = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
-    rotating.duration = 1.5;
-    rotating.fromValue = @0;
-    rotating.toValue = @(M_PI * 2);
-    rotating.repeatCount = HUGE_VAL;
-    [self.layer1 addAnimation:rotating forKey:@"rotating1"];
+    if (animationStage == TwoCirclesActivityAnimationStageStopped) {
+        [self setStartingStage];
+    }
 }
 
 - (void)stopAnimation {
-    [self.layer1 removeAllAnimations];
+    animationStage = TwoCirclesActivityAnimationStageStopping;
 }
+
+#pragma mark animation -
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    switch (animationStage) {
+        case TwoCirclesActivityAnimationStageStarting:
+            [CATransaction setDisableActions:YES];
+            self.layer1.strokeEnd = 1;
+            self.layer2.strokeEnd = 1;
+            [CATransaction setDisableActions:NO];
+
+            [self setRotatingStage];
+
+            break;
+        case TwoCirclesActivityAnimationStageRotating:
+            [self setRotatingStage];
+            break;
+        case TwoCirclesActivityAnimationStageStopping:
+            [CATransaction setDisableActions:YES];
+            self.layer1.strokeStart = 0;
+            self.layer1.strokeEnd = 1;
+
+            self.layer2.strokeStart = 0;
+            self.layer2.strokeEnd = 1;
+            [CATransaction setDisableActions:NO];
+
+            [self setFinalStage];
+            break;
+        case TwoCirclesActivityAnimationStageStopped:
+            [self setInitialState];
+            break;
+    }
+}
+
+- (CABasicAnimation *)rotatingAnimation {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    animation.duration = 1.5;
+    animation.toValue = @0;
+    animation.fromValue = @(M_PI * 2);
+
+    return animation;
+}
+
+- (CABasicAnimation *)ccwRotatingAnimation {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    animation.duration = 1.5;
+    animation.fromValue = @0;
+    animation.toValue = @(M_PI * 2);
+
+    return animation;
+}
+
+-(CABasicAnimation *)startingAnimation {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    animation.duration = 1.3;
+    animation.fromValue = @0;
+    animation.toValue = @1;
+
+    return animation;
+}
+
+-(CABasicAnimation *)stoppingAnimation {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"strokeStart"];
+    animation.duration = 1.3;
+    animation.fromValue = @0;
+    animation.toValue = @1;
+
+    return animation;
+}
+
+
+- (void)setStartingStage {
+    [self.layer1 removeAllAnimations];
+    [self.layer2 removeAllAnimations];
+
+    CABasicAnimation *animation = self.startingAnimation;
+    animation.delegate = self;
+    [self.layer1 addAnimation:animation forKey:@"1"];
+    [self.layer2 addAnimation:self.startingAnimation forKey:@"2"];
+    animationStage = TwoCirclesActivityAnimationStageStarting;
+}
+
+- (void)setRotatingStage {
+    CABasicAnimation *animation = self.rotatingAnimation;
+    animation.delegate = self;
+    [self.layer1 addAnimation:animation forKey:@"1"];
+    [self.layer2 addAnimation:self.ccwRotatingAnimation forKey:@"2"];
+    animationStage = TwoCirclesActivityAnimationStageRotating;
+}
+
+- (void)setFinalStage {
+    CABasicAnimation *animation = self.stoppingAnimation;
+    animation.delegate = self;
+    [self.layer1 addAnimation:animation forKey:@"1"];
+    [self.layer2 addAnimation:self.stoppingAnimation forKey:@"2"];
+    animationStage = TwoCirclesActivityAnimationStageStopped;
+}
+
+- (void)setInitialState {
+    [CATransaction setDisableActions:YES];
+    self.layer1.strokeStart = 0;
+    self.layer1.strokeEnd = 0;
+
+    self.layer2.strokeStart = 0;
+    self.layer2.strokeEnd = 0;
+    [CATransaction setDisableActions:NO];
+}
+
 
 @end
