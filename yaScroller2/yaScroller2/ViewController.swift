d8e619a00d8bec95f2628f3bf36bdd1a9a65cc7c
Victor Zinets <victor.zinets@together.com>
2018-08-29 09:17:59 +0300

    ++ gradients

--- yaScroller2/yaScroller2/ViewController.swift
+++ yaScroller2/yaScroller2/ViewController.swift
@@ -8,9 +8,28 @@
 
 import UIKit
 
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: MediaScrollerView!
+    @IBOutlet weak var gradientProgress: ProgressWithGradient!
     
     
     var items = [PhotoFromInternetModel]()
@@ -41,6 +60,49 @@ class ViewController: UIViewController {
         collectionView.autoScroll = false
         collectionView.tapToScroll = true
         collectionView.endlessScrolling = true
+        
+        
+        // gradients
+        let gradientLayer1 = CAGradientLayer()
+        
+        gradientLayer1.colors = [
+            UIColor(rgb: 0xfec624).cgColor,
+            UIColor(rgb: 0xf161f8).cgColor,
+            UIColor(rgb: 0x7b2df8).cgColor
+        ]
+        gradientLayer1.startPoint = CGPoint(x: 1.0, y: 0.5)
+        gradientLayer1.endPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer1.frame = grad1.bounds
+        
+        grad1.layer.addSublayer(gradientLayer1)
+        
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = grad1.bounds.insetBy(dx: 10, dy: 10)
+        let path = UIBezierPath(ovalIn: maskLayer.bounds)
+                
+        maskLayer.path = path.cgPath
+        maskLayer.fillColor = UIColor.clear.cgColor
+        maskLayer.strokeColor = UIColor.black.cgColor
+        maskLayer.lineWidth = 10
+        maskLayer.lineCap = kCALineCapRound
+        
+        maskLayer.strokeStart = 0.0
+        let pos: CGFloat = 0.95
+        maskLayer.strokeEnd = pos
+        
+        
+        
+        
+        let angle: CGFloat = .pi / 2
+        var transform: CATransform3D = CATransform3DIdentity
+        transform = CATransform3DScale(transform, -1, 1, 1)
+        transform = CATransform3DRotate(transform, angle, 0, 0, 1)
+        
+        maskLayer.transform = transform
+        
+        grad1.layer.mask = maskLayer
+        
+        gradientProgress.position = 0.25
     }
 
     @IBAction func fillCollection(_ sender: Any) {
@@ -58,6 +120,11 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var grad1: UIView!
+    
+    @IBAction func sliderChanges(_ sender: UISlider) {
+        gradientProgress.position = CGFloat(sender.value)
+    }
     
 }
 
---
+++ yaScroller2/yaScroller2/classes/progress with gradient/ProgressWithGradient.swift
@@ -0,0 +1,78 @@
+//
+//  ProgressWithGradient.swift
+//  yaScroller2
+//
+//  Created by Victor Zinets on 8/29/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+import UIKit
+
+class ProgressWithGradient: UIView {
+    
+    func bgLayer() -> CAGradientLayer {
+        let layer = CAGradientLayer()
+        layer.colors = [
+            UIColor(rgb: 0xfec624).cgColor,
+            UIColor(rgb: 0xf161f8).cgColor,
+            UIColor(rgb: 0x7b2df8).cgColor,
+        ]
+        layer.startPoint = CGPoint(x: 1.0, y: 0.5)
+        layer.endPoint = CGPoint(x: 0.0, y: 0.5)
+        layer.frame = self.bounds
+        
+        maskLayer = createMaskLayer()
+        layer.mask = maskLayer!
+        
+        return layer
+    }
+    
+    var maskLayer: CAShapeLayer?
+    func createMaskLayer() -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.frame = self.bounds.insetBy(dx: 5, dy: 5)
+        let path = UIBezierPath(ovalIn: layer.bounds)
+        
+        layer.path = path.cgPath
+        layer.fillColor = UIColor.clear.cgColor
+        layer.strokeColor = UIColor.black.cgColor
+        layer.lineWidth = 10
+        layer.lineCap = kCALineCapRound
+        
+        let angle: CGFloat = .pi / 2
+        var transform: CATransform3D = CATransform3DIdentity
+        transform = CATransform3DScale(transform, -1, 1, 1)
+        transform = CATransform3DRotate(transform, angle, 0, 0, 1)
+        layer.transform = transform
+        
+        return layer
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        self.backgroundColor = UIColor.clear
+        self.layer.addSublayer(bgLayer())
+        position = 0
+    }
+    
+    // MARK: setters -
+    
+    var position: CGFloat = 0 {
+        didSet {
+            self.position = max(0, min(1, position))
+            if let layer = maskLayer {
+                layer.strokeEnd = position
+            }
+        }
+    }
+    
+}
