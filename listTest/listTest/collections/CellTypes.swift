fd9c70e3b5fa8aa172e24937cc1e5a42cb4c4e24
Victor Zinets <victor.zinets@together.com>
2018-09-03 08:21:38 +0300

    ++ new test project

---
+++ listTest/listTest/collections/CellTypes.swift
@@ -0,0 +1,16 @@
+//
+//  CellTypes.swift
+//  listTest
+//
+//  Created by Victor Zinets on 9/3/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+import Foundation
+
+enum CellType {
+    case SearchUser
+    case GeoSearchUser
+    case MessageListItem
+    case SettingsItem
+}
