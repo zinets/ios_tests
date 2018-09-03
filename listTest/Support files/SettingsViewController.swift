9317e8d181cdf82256dfe488a05ea65b111f234d
Victor Zinets <victor.zinets@together.com>
2018-09-03 17:03:30 +0300

    ++ lets make settings (multi-section)

---
+++ listTest/Support files/SettingsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsViewController.swift
+//  listTest
+//
+//  Created by Victor Zinets on 9/3/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeCtrl(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
