a0a4341a437c911cc22cd1d6114439ed75b2749f
Victor Zinets <victor.zinets@together.com>
2018-09-04 13:14:20 +0300

    ++ new project

---
+++ profilePrototyping/profilePrototyping/Supporting files/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  profilePrototyping
+//
+//  Created by Victor Zinets on 9/4/18.
+//  Copyright © 2018 Victor Zinets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func showProfile(_ sender: Any) {
+    }
+    
+}
+
