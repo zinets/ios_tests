80236fbe604c754122064e7de6602fb3c5ba4876
Victor Zinets <victor.zinets@together.com>
2018-09-10 13:16:37 +0300

    ++ pure framework

---
+++ profilePrototyping/Frameworks/CollectionControls.framework/Headers/CollectionControls.h
@@ -0,0 +1,19 @@
+//
+//  CollectionControls.h
+//  CollectionControls
+//
+//  Created by Alexander Dikhtyar on 9/10/18.
+//  Copyright © 2018 TN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CollectionControls.
+FOUNDATION_EXPORT double CollectionControlsVersionNumber;
+
+//! Project version string for CollectionControls.
+FOUNDATION_EXPORT const unsigned char CollectionControlsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CollectionControls/PublicHeader.h>
+
+
