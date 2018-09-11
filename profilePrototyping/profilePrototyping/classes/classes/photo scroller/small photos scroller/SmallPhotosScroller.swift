67befd46bc986fece9c0b91fc1b9a60fb3e66dae
Victor Zinets <victor.zinets@together.com>
2018-09-11 11:36:06 +0300

    ++ скроллер "портретных" фото

--- profilePrototyping/profilePrototyping/classes/classes/photo scroller/small photos scroller/SmallPhotosScroller.swift
+++ profilePrototyping/profilePrototyping/classes/classes/photo scroller/small photos scroller/SmallPhotosScroller.swift
@@ -15,7 +15,7 @@ class SmallPhotosScroller: CollectionBasedScrollerView {
     override open func commonInit() {
         super.commonInit()
         
-        self.paginating = false
+        self.paginating = true
         self.oneElementPaginating = false
     }
     
@@ -26,14 +26,38 @@ class SmallPhotosScroller: CollectionBasedScrollerView {
     override open func layoutForCollection() -> UICollectionViewLayout {
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets.init(top: 0, left: 16, bottom: 0, right: 16)
-        layout.minimumLineSpacing = 10
+        layout.minimumLineSpacing = 8
         
         return layout
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cellWidth: CGFloat = collectionView.numberOfItems(inSection: 0) > 2 ? 130 : 270;
-        return CGSize(width: cellWidth, height: self.bounds.size.height)
+    override open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let wideCell = collectionView.numberOfItems(inSection: 0) > 2
+        let cellWidth: CGFloat = wideCell ? 130 : 267
+        return CGSize(width: cellWidth, height: collectionView.bounds.size.height)
+    }
+    
+    override open func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        // do nothing
+    }
+    
+    override open func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            var targetOffset = targetContentOffset.pointee
+            let proposedOffset = targetOffset.x
+            let cellSize = self.collectionView(scrollView as! UICollectionView, layout: layout, sizeForItemAt:IndexPath(item: 0, section: 0))
+            let pageWidth = cellSize.width + layout.minimumInteritemSpacing
+            var newOffset: CGFloat = 0
+            if scrollView.contentOffset.x + scrollView.bounds.size.width > scrollView.contentSize.width - layout.sectionInset.right {
+                newOffset = scrollView.contentSize.width - scrollView.bounds.size.width
+            } else {
+                let pageIndex = Int((proposedOffset + pageWidth / 2) / pageWidth)
+                newOffset = pageWidth * CGFloat(pageIndex)
+            }
+            
+            targetOffset.x = newOffset
+            targetContentOffset.pointee = targetOffset
+        }
     }
 }
 
