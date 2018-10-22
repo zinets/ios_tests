cc934b32c610d252df0f21ada53eb5b078a7e530
Zinets Victor <victor.zinets@together.com>
2018-10-22 18:23:57 +0300

    ++ initial commit

---
+++ testGradients/testGradients/AnimatedGradientPanel.swift
@@ -0,0 +1,21 @@
+//
+//  AnimatedGradientPanel.swift
+//  testGradients
+//
+//  Created by Zinets Victor on 10/22/18.
+//  Copyright © 2018 Zinets Victor. All rights reserved.
+//
+
+import UIKit
+
+class AnimatedGradientPanel: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
