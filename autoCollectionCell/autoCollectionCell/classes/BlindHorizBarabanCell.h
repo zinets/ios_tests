a8d434a37912b1ceb9f52cfc5e355d272c015a78
Victor Zinets <victor.zinets@together.com>
2018-11-08 12:35:41 +0200

    ++

---
+++ autoCollectionCell/autoCollectionCell/classes/BlindHorizBarabanCell.h
@@ -0,0 +1,17 @@
+//
+//  BlindHorizBarabanCell.h
+//  autoCollectionCell
+//
+//  Created by Victor Zinets on 11/8/18.
+//  Copyright © 2018 TN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BlindHorizBarabanCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ autoCollectionCell/autoCollectionCell/classes/BlindHorizBarabanCell.m
@@ -0,0 +1,18 @@
+//
+//  BlindHorizBarabanCell.m
+//  autoCollectionCell
+//
+//  Created by Victor Zinets on 11/8/18.
+//  Copyright © 2018 TN. All rights reserved.
+//
+
+#import "BlindHorizBarabanCell.h"
+
+@implementation BlindHorizBarabanCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
