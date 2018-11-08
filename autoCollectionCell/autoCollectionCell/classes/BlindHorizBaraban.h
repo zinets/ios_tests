45e135b0ab4e12621cf5513ec9d19a2afa82d3d2
Victor Zinets <victor.zinets@together.com>
2018-11-08 12:33:08 +0200

    ++

---
+++ autoCollectionCell/autoCollectionCell/classes/BlindHorizBaraban.h
@@ -0,0 +1,17 @@
+//
+//  BlindHorizBaraban.h
+//  autoCollectionCell
+//
+//  Created by Victor Zinets on 11/8/18.
+//  Copyright © 2018 TN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BlindHorizBaraban : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ autoCollectionCell/autoCollectionCell/classes/BlindHorizBaraban.m
@@ -0,0 +1,21 @@
+//
+//  BlindHorizBaraban.m
+//  autoCollectionCell
+//
+//  Created by Victor Zinets on 11/8/18.
+//  Copyright © 2018 TN. All rights reserved.
+//
+
+#import "BlindHorizBaraban.h"
+
+@implementation BlindHorizBaraban
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
