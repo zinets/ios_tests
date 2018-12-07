9bf4b9e2188e7475e0c1e504806049529ec853d3
Zinets Viktor <zinetz.victor@gmail.com>
2018-12-07 09:22:35 +0200

    ++ initial commit

---
+++ tapl_navigation/tapl_navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tapl_navigation
+//
+//  Created by Victor Zinets on 12/7/18.
+//  Copyright © 2018 TN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
