84a88fc1043e272e9d895b947f8e41d46e95fbcc
Zinets Viktor <zinetz.victor@gmail.com>
2018-12-17 09:42:38 +0200

    ++ меняем зависимости

--- tapl_navigation/tapl_navigation/nav controllers/TapplNavigationController.swift
+++ tapl_navigation/tapl_navigation/nav controllers/TapplNavigationController.swift
@@ -12,9 +12,42 @@ class TapplNavigationController: UINavigationController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.delegate = self
         
         // .backgroundColor = UIColor(rgb: 0xf9f8f6) // TODO: как получить цвет того бг,
         self.view.backgroundColor = UIColor(rgb: 0xf9f8f6)
     }
 
+}
+
+
+extension TapplNavigationController: UINavigationControllerDelegate {
+    
+    public func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        switch operation {
+        case .push:
+            if let ctrl = toVC as? TapplBaseViewController {
+                ctrl.interactiveAnimator = TapplInteractiveAnimator(attachTo: toVC)
+            }
+            return TapplPushAnimator()
+        case .pop:
+            let popAnimator = TapplPopAnimator()
+            popAnimator.poppingController = fromVC as? TapplBaseViewController
+            return popAnimator
+        default:
+            return nil
+        }
+    }
+    
+    public func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        
+        guard
+            let animator = animationController as? TapplPopAnimator,
+            let ctrl = animator.poppingController,
+            let ia = ctrl.interactiveAnimator
+            else {
+                return nil
+        }
+        return ia.transitionInProgress ? ia : nil
+    }
 }
