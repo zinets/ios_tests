4c1330cd6ea0185e167b381a04aa3950aa756ab3
Zinets Viktor <zinetz.victor@gmail.com>
2019-01-09 16:54:23 +0200

    ++ testing scroller

---
+++ tapl_navigation3/tapl_navigation3/classes/infinite scroller/TapplInfiniteOnboardingScroller.swift
@@ -0,0 +1,21 @@
+//
+//  TapplInfiniteOnboardingScroller.swift
+//  tapl_navigation3
+//
+//  Created by Victor Zinets on 1/9/19.
+//  Copyright © 2019 TN. All rights reserved.
+//
+
+import UIKit
+
+class TapplInfiniteOnboardingScroller: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
