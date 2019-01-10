cdd85e25f9302f80ba8f853c011423d73e29fadb
Zinets Viktor <zinetz.victor@gmail.com>
2019-01-10 08:10:42 +0200

    ++

--- tapl_navigation3/tapl_navigation3/classes/infinite scroller/TapplInfiniteOnboardingScroller.swift
+++ tapl_navigation3/tapl_navigation3/classes/infinite scroller/TapplInfiniteOnboardingScroller.swift
@@ -8,14 +8,104 @@
 
 import UIKit
 
-class TapplInfiniteOnboardingScroller: UICollectionView {
+class TapplInfiniteOnboardingScroller: UIView {
+    
+    static let cellsCount = 15
+    static let cellId = "InfiniteCellID"
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonSetup()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonSetup()
+    }
+    
+    var dataSource: [String] = []
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 8
+        layout.minimumInteritemSpacing = 8
+        layout.sectionInset = UIEdgeInsets(top: 8, left: 0, bottom: 0, right: 0)
+        
+        for i in 1...TapplInfiniteOnboardingScroller.cellsCount {
+            dataSource.append(i.format(f: "02"))
+        }
+        
+        let collectionView = UICollectionView(frame: self.bounds, collectionViewLayout: layout)
+        collectionView.backgroundColor = .black
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.register(TapplInfiniteOnboardingScrollerCell.self, forCellWithReuseIdentifier: TapplInfiniteOnboardingScroller.cellId)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        
+        
+        return collectionView
+    }()
+    
+    private func commonSetup() {
+        self.addSubview(self.collectionView)
+    }
 
+}
+
+extension TapplInfiniteOnboardingScroller: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return TapplInfiniteOnboardingScroller.cellsCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TapplInfiniteOnboardingScroller.cellId, for: indexPath)
+        return cell
+    }
+    
+}
+
+extension Int {
+    func format(f: String) -> String {
+        return String(format: "%\(f)d", self)
+    }
+}
+
+extension TapplInfiniteOnboardingScroller: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if let infiniteCell = cell as? TapplInfiniteOnboardingScrollerCell {
+            let fn = dataSource[indexPath.item]
+            infiniteCell.image = UIImage(named: fn)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 98, height: 148)
+    }
+}
+
+class TapplInfiniteOnboardingScrollerCell: UICollectionViewCell {
+    
+    var image: UIImage? {
+        didSet {
+            imageView.image = image
+        }
+    }
+    private var imageView: UIImageView!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    private func commonInit() {
+        imageView = UIImageView(frame: self.bounds)
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        imageView.contentMode = .scaleAspectFill
+        
+        self.contentView.addSubview(imageView)
+    }
 }
