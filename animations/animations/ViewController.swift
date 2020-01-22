7830202ec008d5229cda78fb9d75ffe05a44e0d8
Зинец Виктор <zinetz.victor@gmail.com>
2020-01-22 15:51:44 +0200

    ++

--- animations/animations/ViewController.swift
+++ animations/animations/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            let layout = CollectionViewPagingLayout()
+            collectionView.setCollectionViewLayout(layout, animated: false)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,3 +24,29 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        return cell
+    }
+    
+    
+}
+
+class TestCell: UICollectionViewCell {
+    
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        if let attrs = layoutAttributes as? CollectionViewPagingLayoutAttributes {
+            topLabel.text = String(format: "%f", attrs.progress)
+            bottomLabel.text = String(format: "%d", attrs.indexPath.item)
+        }
+    }
+    
+}
