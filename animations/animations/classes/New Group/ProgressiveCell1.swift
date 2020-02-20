e3aa4f839320dfce37c5af1957cd1cc9631175ff
Зинец Виктор <zinetz.victor@gmail.com>
2020-02-20 15:20:36 +0200

    ++ вариант стопки с рекогнайзером

--- animations/animations/classes/New Group/ProgressiveCell1.swift
+++ animations/animations/classes/New Group/ProgressiveCell1.swift
@@ -11,16 +11,28 @@ import UIKit
 class ProgressiveCell1: UICollectionViewCell {
     
     @IBOutlet weak var progressLabel: UILabel!
+    private lazy var panRecognizer: UIPanGestureRecognizer = {
+        let pr = UIPanGestureRecognizer(target: self, action: #selector(swipeCard(sender:)))
+        return pr
+    }()
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         self.backgroundColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: 1, brightness: 1, alpha: 1)
+        self.addGestureRecognizer(self.panRecognizer)
     }
+    
+    @objc func swipeCard(sender: UIPanGestureRecognizer) {
+        sender.swipeView()
+    }
+
     
     
     override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
         super.apply(layoutAttributes)
+        
+        self.panRecognizer.isEnabled = layoutAttributes.indexPath.item == 0
         
         guard let attrs = layoutAttributes as? CollectionViewProgressLayoutAttributes else { return }
         progressLabel.text = "progress: \(attrs.progress)"
