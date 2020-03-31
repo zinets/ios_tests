ccb2d4420b6626944c60557ad8732255a7b68f7a
Зинец Виктор <zinetz.victor@gmail.com>
2020-03-31 14:54:47 +0300

    ++ disign

--- protoProfileEdits/protoProfileEdits/cells/CPDOwnProfileEditPickerCell.swift
+++ protoProfileEdits/protoProfileEdits/cells/CPDOwnProfileEditPickerCell.swift
@@ -54,15 +54,31 @@ extension  CPDOwnProfileEditPickerCell: UIPickerViewDelegate, UIPickerViewDataSo
         return dataSource[component].count
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return dataSource[component][row]
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return dataSource[component][row]
+//    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let selectedColor = UIColor(red: 0.008, green: 0.569, blue: 0.953, alpha: 1)
+        let inactiveColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        let color = row == pickerView.selectedRow(inComponent: component) ? selectedColor : inactiveColor
+        let attrString = NSAttributedString(string: dataSource[component][row], attributes: [NSAttributedString.Key.foregroundColor: color])
+        return attrString
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         // я предполагаю, что диапазоны "от" и "до" совпадают, иначе это сплошной геморой и отстутствие смысла от слова "полностьб"
-        if pickerView.numberOfComponents > 0 {
+        if pickerView.numberOfComponents == 2 { // узкий случай выбора диапазона возрастов
             // доп. проверка/выравнивание значений в барабанах
+            if component == 0 && pickerView.selectedRow(inComponent: 1) < row {
+                pickerView.selectRow(row, inComponent: 1, animated: true)
+                pickerView.reloadComponent(1)
+            } else if component == 1 && pickerView.selectedRow(inComponent: 0) > row {
+                pickerView.selectRow(row, inComponent: 0, animated: true)
+                pickerView.reloadComponent(0)
+            }
         }
+        pickerView.reloadComponent(component)
         // TODO: сигналим, что значение поменялось
     }
 }
