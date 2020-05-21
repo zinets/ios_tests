c4e3252636967661674f1339aa1ae6ccbbb01207
Зинец Виктор <zinetz.victor@gmail.com>
2020-05-21 11:13:01 +0300

    ++ initial commit

---
+++ ios13CollectionTests/ios13CollectionTests/IOS13TestViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios13CollectionTests
+//
+//  Created by Viktor Zinets on 21.05.2020.
+//  Copyright © 2020 Viktor Zinets. All rights reserved.
+//
+
+import UIKit
+
+class IOS13TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
