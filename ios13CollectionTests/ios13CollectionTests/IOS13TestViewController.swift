49e0741e91195432deb5d4c4536f59d4a5fc772b
Зинец Виктор <zinetz.victor@gmail.com>
2020-05-21 12:43:01 +0300

    ++ 1й вариант, явно неправильный

--- ios13CollectionTests/ios13CollectionTests/IOS13TestViewController.swift
+++ ios13CollectionTests/ios13CollectionTests/IOS13TestViewController.swift
@@ -15,6 +15,90 @@ class IOS13TestViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // MARK: collection layout -
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.collectionViewLayout = self.createLayout()
+        }
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 0
+        let layout = UICollectionViewCompositionalLayout(sectionProvider: { (section, env) -> NSCollectionLayoutSection? in
+            let section = NSCollectionLayoutSection(group: self.groupFor(section: section))
+            return section
+        }, configuration: config)
+        
+        return layout
+    }
+    
+    
+    
+    func groupFor(section: Int) -> NSCollectionLayoutGroup {
+        switch section {
+        case 0: return group1()
+        default: return group1()
+        }
+    }
+        
+    func group1() -> NSCollectionLayoutGroup {
+        let leadingItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
+                                                     heightDimension: .fractionalWidth(0.5))
+        let leadingItem = NSCollectionLayoutItem(layoutSize: leadingItemSize)
+        leadingItem.contentInsets = .init(top: 4, leading: 4, bottom: 4, trailing: 4)
+        
+        let size2 = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                           heightDimension: .fractionalWidth(0.5))
+        let item2 = NSCollectionLayoutItem(layoutSize: size2)
+        item2.contentInsets = .init(top: 4, leading: 4, bottom: 4, trailing: 4)
 
+        let trailingItemsSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
+                                                       heightDimension: .fractionalHeight(1))
+        let trailingSubGroup = NSCollectionLayoutGroup.vertical(layoutSize: trailingItemsSize,
+                                                                subitem: item2,
+                                                                count: 2)
+        let trailingGroup = NSCollectionLayoutGroup.horizontal(layoutSize:
+            NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
+                                   heightDimension: .fractionalHeight(1)),
+                                                               subitem: trailingSubGroup,
+                                                               count: 2)
+        
+        
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                          heightDimension: .fractionalWidth(0.5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [leadingItem, trailingGroup])
+        return group
+    }
 }
 
+private typealias CollectionDataSource = IOS13TestViewController
+extension CollectionDataSource: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0, 1, 2:
+            return 5
+        default:
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch indexPath.section {
+        case 0, 1, 2:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath)
+            
+            return cell
+        default:
+            fatalError()
+        }
+    }
+    
+    
+}
---
+++ ios13CollectionTests/ios13CollectionTests/TestCell1.swift
@@ -0,0 +1,18 @@
+//
+//  TestCell1.swift
+//  ios13CollectionTests
+//
+//  Created by Viktor Zinets on 21.05.2020.
+//  Copyright © 2020 Viktor Zinets. All rights reserved.
+//
+
+import UIKit
+
+class TestCell1: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.apply(layoutAttributes)
+        label.text = String(format: "%d : %d", layoutAttributes.indexPath.section, layoutAttributes.indexPath.item)
+    }
+}
